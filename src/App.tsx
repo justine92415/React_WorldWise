dfa1c4109d4b6f05a738def0479fec729a5a5863
justine92415 <justine92415@gmail.com>
2025-02-22 17:07:22 +0800

    feat:210.新增應用程式佈局、導航元件及登入頁面樣式

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Product from './pages/Product';
-import Pricing from './pages/Pricing';
-import Homepage from './pages/Homepage';
-import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
+import Homepage from './pages/Homepage';
+import Product from './pages/Pricing';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="login" element={<Login />}></Route>
         <Route path="app" element={<AppLayout />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
